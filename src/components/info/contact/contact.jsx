4218aa5a09d52d499eaa64a5e855fc74923e88e8
Masson <morgan.masson006@gmail.com>
2024-11-18 22:47:10 +0100

    modification page contact

--- src/components/info/contact/contact.jsx
+++ src/components/info/contact/contact.jsx
@@ -1,28 +1,27 @@
 import '../contact/style/contact.css'
-import imageGithub from '../../../assets/svg/contact/github-brands-solid.svg'
+import imageGithub from '../../../assets/svg/skills/logo/github.svg'
 import imageLinkedIn from '../../../assets/svg//contact/linkedin-brands-solid.svg'
+import imagePhone from '../../../assets/svg/contact/phone.svg'
+import imageMail from '../../../assets/svg/contact/mail.svg'
 
 function Contact (){
     return(
         <div id="contact" className='side-div'>
-            <h2>Contact</h2>
-            <div id='contact-app'>
-                <a href="https://www.linkedin.com/in/morgan-masson/" target='_blank' id='linkedin'><img src= {imageLinkedIn} alt="logo LinkedIn" /></a>
-                <a href="https://github.com/M-Masson" target='_blank' id='github'><img src= {imageGithub} alt="logo GitHub" /></a>
-            </div>
-            <div id='contact-infos'>
-                <div id='email'>
-                    <p className='infos-title'>Email:</p>
-                    <p className='infos-txt'>morgan.masson006@gmail.com</p>
+            <div id="contact-page">
+                <h2>Contact</h2>
+                <div id='contact-app'>
+                        <a href="https://www.linkedin.com/in/morgan-masson/" target='_blank' id='linkedin' className='contact-elements'>
+                            <img src= {imageLinkedIn} alt="logo LinkedIn" />
+                        </a>
+                        <a href="https://github.com/M-Masson" target='_blank' id='github' className='contact-elements'>
+                            <img src= {imageGithub} alt="logo GitHub" />
+                        </a>
                 </div>
-                <div id='phone'>
-                    <p className='infos-title'>Téléphone:</p>
-                    <p className='infos-txt'>+33 7 82 37 43 16</p>
+                <div id='contact-join'>
+                    <a id='mail' className='contact-elements' href="mailto:morgan.masson006@gmail.com"><img src={imageMail} alt="logo mail" /></a>
+                    <a id='phone' href="" className='contact-elements'><img src={imagePhone} alt="logo phone" /></a>
                 </div>
             </div>
-           
-                <a id='contact-me' href="mailto:morgan.masson006@gmail.com">Contactez-moi</a>
-            
         </div>
     )
 }
