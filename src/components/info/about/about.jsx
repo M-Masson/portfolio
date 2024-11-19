f2ed985c2d9c24c754a3a8cbd597d627d6d60787
Masson <morgan.masson006@gmail.com>
2024-11-19 18:21:30 +0100

    ajout de texte à propos

--- src/components/info/about/about.jsx
+++ src/components/info/about/about.jsx
@@ -3,9 +3,33 @@ import '../about/style/about.css'
 function About(){
     return(
         <div id="about" className='side-div'>
-            <div id='about1'>
-                <div>Bio</div>
-                <div>Domaine</div>
+            <div id='about-all'>
+                <h2>À Propos</h2>
+                <div id='about1'>
+                    <div>
+                        <h3>Biographie</h3>
+                        <p>
+                            Actuellement en formation de développeur web avec l’organisme OpenClassrooms,
+                            je me spécialise dans la création de sites et d’applications web modernes. <br /> 
+                            Diplômé d’un baccalauréat général avec une spécialisation en Mathématiques 
+                            et Sciences de l’Ingénieur, j’ai toujours été attiré par la résolution de 
+                            problèmes techniques et logiques. <br /> Passionné par l’informatique et les jeux vidéo,
+                            je puise mon inspiration dans ces domaines pour concevoir des projets innovants 
+                            et immersifs.
+                        </p>
+                    </div>
+                    <div>
+                        <h3>Pour le futur</h3>
+                        <p>
+                        Accordant une grande importance à l’ergonomie, à la performance et à l’esthétique
+                        des projets sur lesquels je travaille, je souhaite me spécialiser dans le domaine
+                        du développement Front-End. <br /> Toujours en quête de nouvelles compétences,
+                        chaque projet est une opportunité pour innover, résoudre des problèmes
+                        et apprendre de nouvelles technologies. <br /> J’aime relever des défis et transformer
+                        des idées en expériences digitales uniques.
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
     )
