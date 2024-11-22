cadf78921919dee5661d8230a1c637af7cdeb00e
Masson <morgan.masson006@gmail.com>
2024-11-22 15:17:46 +0100

    modification path route.jsx

--- src/route.jsx
+++ src/route.jsx
@@ -2,15 +2,17 @@ import Error from './pages/error';
 import Home from './pages/home';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+const path = "/portfolio"
+
 function RoutesApp (){
     return(
     <Router>
             <Routes>
-                <Route path="/" element={<Home />}/>
-                <Route path="/abouts" element={<Home />}/>
-                <Route path="/skills" element={<Home />}/>
-                <Route path="/projects" element={<Home />}/>
-                <Route path="/contacts" element={<Home />}/>
+                <Route path={`${path}/`} element={<Home />}/>
+                <Route path={`${path}/abouts`} element={<Home />}/>
+                <Route path={`${path}/skills`} element={<Home />}/>
+                <Route path={`${path}/projects`} element={<Home />}/>
+                <Route path={`${path}/contacts`} element={<Home />}/>
                 <Route path ="*" element={<Error/>}/>
             </Routes>
     </Router>
